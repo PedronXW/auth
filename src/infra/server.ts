8d1e306ff181f487cd7d5fe1f85933053b94ee57
pedronxw <pedroalmeidan@gmail.com>
2024-03-13 22:02:08 -0300

    Remove RabbitMQModule and related code from server.ts

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,23 +1,9 @@
 import { setupEvents } from '@/domain/enterprise/events'
 import { env } from './env'
 import { app } from './http/app'
-import { RabbitMQModule } from './rabbitmq/rabbitmqModule'
 
 setupEvents()
 
 app.listen(env.PORT, async () => {
-  const rabbit = new RabbitMQModule()
-  await rabbit.connect('amqp://rabbitmq')
-  rabbit.consume(
-    {
-      exchange: 'client',
-      exchangeType: 'fanout',
-      queue: 'client-created',
-    },
-    (msg) => {
-      console.log(`New client created: ${msg?.content.toString()}`)
-    },
-  )
-
   console.log(`Server listening on port ${env.PORT}`)
 })
